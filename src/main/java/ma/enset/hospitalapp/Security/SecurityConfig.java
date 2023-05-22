4c556961f73a1b0d538f3839a370353fb2c5bb63
TARAR Ayman <83256011+spyhimself@users.noreply.github.com>
2023-05-22 12:54:11 +0100

    Added some InMemory Users in order to use them in the authentication fromLogin

---
+++ src/main/java/ma/enset/hospitalapp/Security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package ma.enset.hospitalapp.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password("{noop}1234").roles("USER").build(),
+                User.withUsername("user2").password("1234").roles("USER").build(),
+                User.withUsername("admin").password("notadmin").roles("USER", "ADMIN").build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin();
+        httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+        return httpSecurity.build();
+    }
+}
